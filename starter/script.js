d2006c109408a2e936a8e209a0fd94d7257ef63d
Arash-Ra <rahebiarash@gmail.com>
2021-11-03 21:34:44 -0400

    commit 2021-11-03-21-34

--- starter/script.js
+++ starter/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -97,4 +97,33 @@ console.log(arr);
 
 // Reverse
 arr = ['a', 'b', 'c', 'd', 'e'];
-let arr2 = ['j', 'i', 'h', 'g', 'j'];
+let arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+
+// Concat
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+// Join
+console.log(letters.join(' - '));
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+console.log(`------- FOR OF -------`);
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`${i + 1}: You deposited ${movement}`);
+  } else console.log(`${i + 1}: You withdrew ${Math.abs(movement)}`);
+}
+
+// If we want to break the loop we need to use for of
+// We cannot break the forEach loop
+
+console.log(`------- FOREACH -------`);
+
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`${i + 1}: You deposited ${mov}`);
+  } else console.log(`${i + 1}: You withdrew ${Math.abs(mov)}`);
+});
