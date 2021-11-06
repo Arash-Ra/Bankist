bce11ca0cfb47a8d8b1a6a9a165c9fa54d40fe33
Arash-Ra <rahebiarash@gmail.com>
2021-11-05 22:42:22 -0400

    commit 2021-11-05-22-41

--- starter/script.js
+++ starter/script.js
@@ -65,12 +65,6 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
@@ -126,4 +120,25 @@ movements.forEach(function (mov, i, arr) {
   if (mov > 0) {
     console.log(`${i + 1}: You deposited ${mov}`);
   } else console.log(`${i + 1}: You withdrew ${Math.abs(mov)}`);
+});
+
+// Map
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(` ${key}: ${value}`);
+});
+
+// Set
+
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+
+currenciesUnique.forEach(function (value, key) {
+  console.log(`${key}: ${value}`);
 });
