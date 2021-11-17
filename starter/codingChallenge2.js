9de5d85de6154f6680fa3142751ed8597924b693
Arash-Ra <rahebiarash@gmail.com>
2021-11-16 22:28:14 -0500

    commit 2021-11-16-22-27

--- starter/codingChallenge2.js
+++ starter/codingChallenge2.js
@@ -1,13 +1,20 @@
 'use strict';
 
-let dogsAges = [2, 3, 4, 1];
-let dogInHumanAge = dogsAges.map(function (dgAge) {
-  if (dgAge <= 2) {
-    return dgAge * 2;
-  } else {
-    return dgAge * 4;
-  }
-});
+let dogsAges = [5, 2, 4, 1, 15, 8, 3];
+
+const calcAverageHumanAge = function (ages) {
+  const converted = ages.map(age => (age <= 2 ? age * 2 : 16 + age * 4));
+  console.log(converted);
+
+  // Excluding the ages below 18
+  const adult = converted.filter(age1 => age1 > 18);
+  console.log(adult);
+
+  // Calculating the adult ages average
+  // Average
+  const avg = adult.reduce((acc, cur) => acc + cur, 0) / adult.length;
+  console.log(avg);
+};
 
 console.log(`Original array: ${dogsAges}`);
-console.log(`Converted to human age: ${dogInHumanAge}`);
+calcAverageHumanAge(dogsAges);
